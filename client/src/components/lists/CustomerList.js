ad5c2b314f3ec2499be6ec9c8579dbb72f297419
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-03-02 16:52:56 -0700

    add customer list

---
+++ client/src/components/lists/CustomerList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CustomerList() {
+  return (
+    <div>
+      <ul>
+        <li>List item</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CustomerList;
