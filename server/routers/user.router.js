646803a9af7989e1158f165708d743d472903ba1
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-03-03 19:54:48 -0700

    add new router

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+// dfasd
 // Register user
 router.post('/', async (req, res) => {
   try {
